9f75ca2db1c541b493d8e1b26ae630921818f95d
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-02-04 16:11:21 -0600

    .

--- DataClass.py
+++ DataClass.py
@@ -6,14 +6,10 @@ class Data:
         self.um = np.zeros((size))
         self.ui = np.zeros((size))
         self.up = np.zeros((size))
-        self.u_min = np.zeros((size))
-        self.u_max = np.zeros((size))
         self.dudt = np.zeros((size))
 
     def collect(self,um,ui,up,dudt):
         self.um = np.append(self.um,um)
         self.ui = np.append(self.ui,ui)
         self.up = np.append(self.up,up)
-        self.u_min = np.append(self.u_min,np.min([0,ui]))
-        self.u_max = np.append(self.u_max,np.max([0,ui]))
         self.dudt = np.append(self.dudt,dudt)
--- pymc_burgers.py
+++ pymc_burgers.py
@@ -1,56 +1,52 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from IPython.display import display
 from BurgersClass import Burgers
 from DataClass import Data
+import random
+import matplotlib.pyplot as plt
 import arviz as az
 az.style.use("arviz-darkgrid")
+from IPython.display import display
 import pymc3 as pm
-import random
+import theano.tensor as tt
+import warnings
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
-# generate and collect data
+# solve & collect dat
 dat = Data()
-fom = Burgers(grid_size=101,dt=0.0005,nu=0.1)
+fom = Burgers(grid_size=101,dt=0.0005,nu=0.2)
 for i in range(1001):
     fom.up_dudt()
     if(i%50==0):
         dat.collect(fom.u[74],fom.u[75],fom.u[76]\
-                ,fom.dudt[75]+random.uniform(-0.05, 0.05))
+                ,fom.dudt[75]*(1+random.uniform(-0.05, 0.05)))
     fom.up_u()
 
+# define model
 basic_model = pm.Model()
-
-
-
 with basic_model:
     # Priors for unknown model parameters
-    nu = pm.Normal("nu", mu=5, sigma=5)
-    sigma = pm.HalfNormal("sigma", sigma=0.5)
+    nu = pm.Normal("nu", mu=10, sigma=10)
+    um = dat.um; ui = dat.ui; up = dat.up; dx = fom.dx
 
-    um = dat.um; ui = dat.ui; up = dat.up; umax = dat.u_max; umin = dat.u_min
-    dx = fom.dx
     # Expected value of outcome
-    mu = umax*(um-ui)/dx + umin*(ui-up)/dx + nu*(up-2*ui+um)/dx**2
+    umax = tt.switch((ui>0).all(),ui,0)
+    umin = tt.switch((ui<0).all(),ui,0)
+    dudt = umax*(um-ui)/dx + umin*(ui-up)/dx + nu*(up-2*ui+um)/dx**2
 
     # Likelihood (sampling distribution) of observations
-    Y_obs = pm.Normal("Y_obs", mu=mu, sigma=sigma, observed=dat.dudt)
+    pm.Normal("y", mu=dudt,observed=dat.dudt)
 
-map_estimate = pm.find_MAP(model=basic_model)
-map_estimate
-
+# trace/sample model
 with basic_model:
-    # draw 500 posterior samples
-    trace = pm.sample(500, return_inferencedata=False, chains=2)
+    trace = pm.sample(500, chains=2)
 
-trace["nu"][-5:]
-
+# plot outputs
 with basic_model:
-    az.plot_trace(trace);
-mngr = plt.get_current_fig_manager()
-mngr.window.setGeometry(0,-500,600,400)
+    pm.plot_trace(trace) # pm.plot_trace(trace,['nu','sigma'])
+    mngr = plt.get_current_fig_manager()
+    mngr.window.setGeometry(0,-500,600,400) #(left,top,width,height)
+    plt.show()
 
-plt.show()
-
+print('last five nu samples:',trace["nu"][-5:])
 with basic_model:
     display(az.summary(trace, round_to=2))
-#problem: in func I create new variables for np.max([0,ui]) & np.min([0,ui])
