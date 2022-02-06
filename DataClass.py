9db589509576d23d7fabd15ff31399d24853b795
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-02-06 15:00:51 -0600

    DataClass updated

--- DataClass.py
+++ DataClass.py
@@ -2,14 +2,10 @@ import numpy as np
 print()
 
 class Data:
-    def __init__(self,size=0):
-        self.um = np.zeros((size))
-        self.ui = np.zeros((size))
-        self.up = np.zeros((size))
-        self.dudt = np.zeros((size))
+    def __init__(self,size):
+        self.u = np.zeros((0,size));
+        self.dudt = np.zeros((0,size))
 
-    def collect(self,um,ui,up,dudt):
-        self.um = np.append(self.um,um)
-        self.ui = np.append(self.ui,ui)
-        self.up = np.append(self.up,up)
-        self.dudt = np.append(self.dudt,dudt)
+    def collect(self,u,dudt):
+        self.u = np.vstack((self.u,u))
+        self.dudt = np.vstack((self.dudt,dudt))
--- pymc_burgers.py
+++ pymc_burgers.py
@@ -11,30 +11,29 @@ import theano.tensor as tt
 import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-# solve & collect dat
-dat = Data()
+# solve & collect data
+data = Data(101)
 fom = Burgers(grid_size=101,dt=0.0005,nu=0.2)
 for i in range(1001):
     fom.up_dudt()
-    if(i%50==0):
-        dat.collect(fom.u[74],fom.u[75],fom.u[76]\
-                ,fom.dudt[75]*(1+random.uniform(-0.05, 0.05)))
+    if(i%100==0):
+        data.collect(fom.u,fom.dudt)
     fom.up_u()
+
 
 # define model
 basic_model = pm.Model()
 with basic_model:
     # Priors for unknown model parameters
     nu = pm.Normal("nu", mu=10, sigma=10)
-    um = dat.um; ui = dat.ui; up = dat.up; dx = fom.dx
 
     # Expected value of outcome
-    umax = tt.switch((ui>0).all(),ui,0)
-    umin = tt.switch((ui<0).all(),ui,0)
+    um = data.u[:,74]; ui = data.u[:,75]; up = data.u[:,76]; dx = fom.dx
+    umax = tt.switch((ui>0).all(),ui,0); umin = tt.switch((ui<0).all(),ui,0)
     dudt = umax*(um-ui)/dx + umin*(ui-up)/dx + nu*(up-2*ui+um)/dx**2
 
     # Likelihood (sampling distribution) of observations
-    pm.Normal("y", mu=dudt,observed=dat.dudt)
+    pm.Normal("y", mu=dudt,observed=data.dudt[:,75])
 
 # trace/sample model
 with basic_model:
